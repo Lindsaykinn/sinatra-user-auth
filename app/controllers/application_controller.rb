c756758ef1406fa17a86f699d8b6758d9113720b
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-12-23 18:30:54 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,25 +11,29 @@ class ApplicationController < Sinatra::Base
     erb :home
   end
 
+  #renders the sign-up form view
   get '/registrations/signup' do
 
     erb :'/registrations/signup'
   end
 
+  #responsible for handling the POST request that is sent when a user hits 'submit' on the sign-up form. It will contain code that gets the new user's info from the params hash, creates a new user, signs them in, and then redirects them somewhere else.
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
-    session[:user_id] = @user.id
+    session[:user_id] = @user.id    
 
     redirect '/users/home'
   end
 
+  #responsible for rendering the login form
   get '/sessions/login' do
 
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
+  #responsible for receiving the POST request that gets sent when a user hits 'submit' on the login form. This route contains code that grabs the user's info from the params hash, looks to match that info against the existing entries in the user database, and, if a matching entry is found, signs the user in.
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
@@ -39,11 +43,13 @@ class ApplicationController < Sinatra::Base
     redirect '/sessions/login'
   end
 
+  #responsible for logging the user out by clearing the session hash.
   get '/sessions/logout' do
     session.clear
     redirect '/'
   end
 
+  #responsible for rendering the user's homepage view
   get '/users/home' do
 
     @user = User.find(session[:user_id])
